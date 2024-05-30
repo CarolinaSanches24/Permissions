6cc91ad628da3a01201ef9f081e151f35e4c7a65
CAROL SANCHES <135077050+CarolinaSanches24@users.noreply.github.com>
2024-05-30 04:08:05 -0300

    feat:baseRepo

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,7 +1,33 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { BaseRepo } from '../../services/core/baseRepo';
+import { userSchema } from '../db/schemas/user/userSchema';
 
 const app = express();
+app.use(express.json());
 
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000');
+class UserRepo extends BaseRepo{
+  constructor() {
+    super(userSchema);
+  }
+
+  public async create(data: object): Promise<any> {
+    return super.create(data);
+  }
+}
+
+const userRepo = new UserRepo();
+
+app.post('/test-insert', async (req: Request, res: Response) => {
+  try {
+    const data = req.body;
+    const result = await userRepo.create(data);
+    res.status(201).send(`Dados inseridos com sucesso! ID: ${result.insertId}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao inserir dados');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado na porta 3000');
 });
---
+++ src/services/core/baseRepo.ts
@@ -0,0 +1,25 @@
+import { MySqlTable } from "drizzle-orm/mysql-core";
+import { db } from "../../infra/db/connection";
+
+export abstract class BaseRepo{
+    private table : MySqlTable;
+
+    constructor(table:MySqlTable) {
+        this.table = table;
+
+    }
+    
+  public async create(data: object): Promise<any> {
+        const result = await db.insert(this.table).values(data).execute();
+        return result;
+          
+    }
+
+    // async update(id: number, data:object): Promise<void> {
+    //     await db.update(this.table).set(data).where('id');
+    // }
+
+    // async delete(id: number): Promise<void> {
+    //    await db.delete().from(this.tableConfig.name).where({ id });
+    // }
+}
